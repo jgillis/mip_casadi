de61156fb1db3f057c02342b4345a954733eb5b0
Joris Gillis <joris.gillis42@gmail.com>
2023-11-06 15:19:18 +0100

    gurobi and cbc

--- demo.py
+++ demo.py
@@ -1,25 +1,43 @@
 import casadi as ca
+import pandas as pd
+import numpy as np
 
 file_path = "neos-860300.pkl"
-
-
-# Solved with DiveCoefficient: mas76 binkar10_1
 
 import pickle
 kwargs = pickle.load(open(file_path,'rb'))
 
+common_options = {"discrete": kwargs["discrete"],"print_time":False}
 
-#solver = ca.conic('solver','cbc',{"a":kwargs["a"].sparsity()},{"cbc":{"threads":0},"discrete": kwargs["discrete"],"print_time":True})
+def get_solvers(n_threads):
+    yield ("gurobi",{"gurobi":{"threads": n_threads}})
+    yield ("cbc",{"cbc":{"threads": n_threads}})
+    
+results = []
 
+for n_threads in [1,2,4,8]:
+    for solver_name,solver_options in get_solvers(n_threads):
+        options = dict(common_options)
+        options.update(solver_options)
+        
+        solver = ca.conic('solver',solver_name,{"a":kwargs["a"].sparsity()},options)
+        
+        sol = solver(lbx=kwargs["lbx"],ubx=kwargs["ubx"],lba=kwargs["lba"],uba=kwargs["uba"],a=kwargs["a"],g=kwargs["g"])
+        
+        assert int(np.round(sol["cost"]))==3201 # See https://plato.asu.edu/ftp/milp_res8/benchmark.cbc.8threads.7200s.res
+        
+        stats = solver.stats()
+        print(stats)
+        
+        results.append({
+            "solver": solver_name,
+            "threads": n_threads,
+            "proc": stats['t_proc_solver'],
+            "wall": stats['t_wall_solver']
+        })
 
-solver = ca.conic('solver','highs',{"a":kwargs["a"].sparsity()},{"discrete": kwargs["discrete"],"print_time":True})
+results_df = pd.DataFrame(results)
 
+results_df.set_index(['solver', 'threads'], inplace=True)
 
-sol = solver(lbx=kwargs["lbx"],ubx=kwargs["ubx"],lba=kwargs["lba"],uba=kwargs["uba"],a=kwargs["a"],g=kwargs["g"])
-
-assert sol["cost"]==3201 # See https://plato.asu.edu/ftp/milp_res8/benchmark.cbc.8threads.7200s.res
-
-stats = solver.stats()
-
-print(stats["t_proc_total"],stats["t_wall_total"])
-#print(stats[""])
+print(results_df)
